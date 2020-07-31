e77e19cd1e2890069767f26a2af9df2b4f9f2cbd
Nicolás Mann <nicolas.mann@alumnos.uach.cl>
2020-07-30 22:11:13 -0400

    Funcionalidad Basica

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,73 @@ import { css } from "@emotion/react";
 import { useState } from "react";
 import data from "../hispadic1207.json";
 
-const Resultado = () => {
-  return <div>Lista con los resultados</div>;
+const Resultado = (props) => {
+  return (
+    <>
+      <div>Lista con los resultados para {props.input}</div>
+      <ol>
+        {props.datos.map((resultado) =>
+          <li>{resultado.japanese} ({resultado.reading}): {resultado.spanish}</li>
+        )}
+      </ol>
+
+      <button
+        onClick = {() => props.setResultados(null)}
+      >
+        Atrás
+      </button>
+    </>
+  );
 };
+
+const pedirInformacion = (input, setResultados) => {
+  const resultados = []
+  data.forEach((entrada) => {
+    if (entrada.japanese.includes(input) || entrada.spanish.includes(input)) resultados.push(entrada);
+  })
+  
+  setResultados(resultados)
+}
+
+const Query = (props) => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: row;
+        margin: 20px;
+        justify-content: center;
+        gap: 1%;
+      `}
+    >
+      <input
+        css={css`
+          border: 1px solid #4d4d4d;
+          font-size: 1.2em;
+          color: #1a1a1a;
+        `}
+        placeholder="Ingrese su busqueda"
+        value={props.texto}
+        onChange={(e) => {props.setTexto(e.target.value)}}
+      />
+      <button
+        css={css`
+          font-size: 1em;
+          background-color: white;
+          color: #4d4d4d;
+          border: 1px solid #4d4d4d;
+        `}
+        onClick = {() => pedirInformacion(props.texto, props.setResultados)}
+      >
+        Buscar
+      </button>
+    </div>
+  )
+}
 
 const Main = () => {
   const [texto, setTexto] = useState("青空");
+  const [resultados, setResultados] = useState(null);
   return (
     <>
       <div
@@ -36,36 +97,10 @@ const Main = () => {
         alt="anime-girl"
         src="/test.png"
       />
-      <div
-        css={css`
-          display: flex;
-          flex-direction: row;
-          margin: 20px;
-          justify-content: center;
-          gap: 1%;
-        `}
-      >
-        <input
-          css={css`
-            border: 1px solid #4d4d4d;
-            font-size: 1.2em;
-            color: #1a1a1a;
-          `}
-          placeholder="Ingrese su busqueda"
-          value={texto}
-          onChange={(e) => {}}
-        />
-        <button
-          css={css`
-            font-size: 1em;
-            background-color: white;
-            color: #4d4d4d;
-            border: 1px solid #4d4d4d;
-          `}
-        >
-          Buscar
-        </button>
-      </div>
+      {resultados
+        ? <Resultado datos={resultados} setResultados={setResultados} input={texto}/>
+        : <Query texto={texto} setTexto={setTexto} setResultados={setResultados}/>
+      }
     </>
   );
 };
