d62a567fef0aee3728c5b17913e2f1616f315f31
Javier R <nadie514@gmail.com>
2020-07-31 02:24:27 -0400

    Added css to new elements
    style added to new elements from last commit

--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,100 @@ import data from "../hispadic1207.json";
 const Resultado = (props) => {
   return (
     <>
-      <div>Lista con los resultados para {props.input}</div>
-      <ol>
-        {props.datos.map((resultado) =>
-          <li>{resultado.japanese} ({resultado.reading}): {resultado.spanish}</li>
-        )}
-      </ol>
-
-      <button
-        onClick = {() => props.setResultados(null)}
+      <div
+        css={css`
+          text-indent: 30px;
+        `}
       >
-        Atrás
-      </button>
+        Resultados de {props.input}
+      </div>
+      <ul
+        css={css`
+          display: flex;
+          flex-direction: column;
+          list-style-type: none;
+          align-items: center;
+        `}
+      >
+        {props.datos.map((resultado) => (
+          <li
+            css={css`
+              height: 70px;
+              width: 80%;
+              line-height: 70px;
+              display: flex;
+              flex-direction: row;
+              list-style-type: none;
+              justify-content: left;
+              border: solid 1px;
+            `}
+          >
+            <span
+              css={css`
+                text-indent: 10px;
+                color: black;
+              `}
+            >
+              {resultado.japanese}
+            </span>
+            <span
+              css={css`
+                text-indent: 10px;
+                font-size: 0.7em;
+                color: #4d4d4d;
+              `}
+            >
+              ({resultado.reading})
+            </span>
+            <span
+              css={css`
+                text-indent: 10px;
+                color: #333333;
+              `}
+            >
+              {resultado.spanish}
+            </span>
+            <div
+              css={css`
+                margin: auto;
+                align-self: right;
+                display: flex;
+                flex-direction: column;
+                align-items: right;
+              `}
+            >
+              <button
+                css={css`
+                  height: 30px;
+                  border: none;
+                  color: #333333;
+                `}
+              >
+                + Info
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
 
 const pedirInformacion = (input, setResultados) => {
-  const resultados = []
+  const resultados = [];
   data.forEach((entrada) => {
-    if (entrada.japanese.includes(input) || entrada.spanish.includes(input)) resultados.push(entrada);
-  })
-  
-  setResultados(resultados)
-}
+    if (entrada.japanese.includes(input) || entrada.spanish.includes(input))
+      resultados.push(entrada);
+  });
+
+  setResultados(resultados);
+};
 
 const Query = (props) => {
   return (
     <div
       css={css`
+        height: 40px;
         display: flex;
         flex-direction: row;
         margin: 20px;
@@ -49,7 +114,9 @@ const Query = (props) => {
         `}
         placeholder="Ingrese su busqueda"
         value={props.texto}
-        onChange={(e) => {props.setTexto(e.target.value)}}
+        onChange={(e) => {
+          props.setTexto(e.target.value);
+        }}
       />
       <button
         css={css`
@@ -58,13 +125,13 @@ const Query = (props) => {
           color: #4d4d4d;
           border: 1px solid #4d4d4d;
         `}
-        onClick = {() => pedirInformacion(props.texto, props.setResultados)}
+        onClick={() => pedirInformacion(props.texto, props.setResultados)}
       >
         Buscar
       </button>
     </div>
-  )
-}
+  );
+};
 
 const Main = () => {
   const [texto, setTexto] = useState("青空");
@@ -74,6 +141,9 @@ const Main = () => {
       <div
         css={css`
           text-align: center;
+          height: 130px;
+          line-height: 130px;
+          align-items: center;
           background-image: linear-gradient(
             180deg,
             rgba(0, 0, 51, 1) 2%,
@@ -85,26 +155,31 @@ const Main = () => {
           font-weight: bold;
         `}
       >
+        <img
+          css={css`
+            width: 90px;
+            margin: auto auto;
+            display: inline-flex;
+            align-content: center;
+            justify-content: center;
+          `}
+          alt="anime-girl"
+          src="/test.png"
+        />
         Diccionario 辞書
       </div>
-      <img
-        css={css`
-          width: 130px;
-          margin: 0px auto;
-          display: flex;
-          align-content: center;
-        `}
-        alt="anime-girl"
-        src="/test.png"
-      />
-      {resultados
-        ? <Resultado datos={resultados} setResultados={setResultados} input={texto}/>
-        : <Query texto={texto} setTexto={setTexto} setResultados={setResultados}/>
-      }
+      <Query texto={texto} setTexto={setTexto} setResultados={setResultados} />
+      {resultados ? (
+        <Resultado
+          datos={resultados}
+          setResultados={setResultados}
+          input={texto}
+        />
+      ) : null}
     </>
   );
 };
 
-const c = "";
+const c = ""; //Importante. No borrar
 
 export default Main;
